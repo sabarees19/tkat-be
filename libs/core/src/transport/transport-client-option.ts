8a45d8091d90af7c3a2c68ab6da523c29e57f8fd
astraops-sabarees <sabareesv@astraops.ai>
2025-06-17 20:02:28 +0530

    change1

--- libs/core/src/transport/transport-client-option.ts
+++ libs/core/src/transport/transport-client-option.ts
@@ -2,15 +2,27 @@ import { ClientOptions, Transport } from '@nestjs/microservices';
 import { EnvService } from '../env/env.service';
 
 export function getRedisTransportOption(envService: EnvService) {
+  const options: ClientOptions = {
+    transport: Transport.REDIS,
+    options: {
+      host: envService.get('REDIS_HOST') || process.env['REDIS_HOST'],
+      port:
+        parseInt(envService.get('REDIS_PORT')) ||
+        parseInt(process.env['REDIS_PORT'] as string),
+      username:
+        envService.get('REDIS_USERNAME') || process.env['REDIS_USERNAME'],
+      password:
+        envService.get('REDIS_PASSWORD') || process.env['REDIS_PASSWORD'],
+    },
+  };
+  console.log(options.options);
   return {
     transport: Transport.REDIS,
     options: {
-      host: process.env['REDIS_HOST'] || envService.get('REDIS_HOST'),
-      port: parseInt(process.env['REDIS_PORT'] || envService.get('REDIS_PORT')),
-      username:
-        process.env['REDIS_USERNAME'] || envService.get('REDIS_USERNAME'),
-      password:
-        process.env['REDIS_PASSWORD'] || envService.get('REDIS_PASSWORD'),
+      host: envService.get('REDIS_HOST'),
+      port: parseInt(envService.get('REDIS_PORT')),
+      username: envService.get('REDIS_USERNAME'),
+      password: envService.get('REDIS_PASSWORD'),
     },
   } as ClientOptions;
 }
